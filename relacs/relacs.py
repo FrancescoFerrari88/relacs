2e8735cbfc2a134a2733e884b0c56c30638441fc
Francesco Ferrari <francesco.ferrari.1988@gmail.com>
2020-02-09 01:35:11 +0100

    created init method for class makeRelacsObject

--- relacs/relacs.py
+++ relacs/relacs.py
@@ -1,62 +1,119 @@
-def add_nums(x,y):
-    try:
-        x = float(x)
-        y = float(y)
-    except ValueError as exp_type:
-        print("you have to provide numbers to add: {}".format(exp_type))
-    return float(x)+float(y)
+"""
 
-def divide_nums(x,y):
-    if y == 0:
-        raise ValueError("Cannot divide by zero!")
-    else:
-        return x / y
+logic of relacs module:
 
-class Employees:
+tasks to perform:
 
-    raise_amt = 1.05
+1. compute RELACS scaling factors (either from total number of reads or from a set of bed regions)
+ - compute various kinds of scaling factors (RLE (deseq2), and other deeptools SF)
+2. plot scalign factor heatmap for a given mark
+3. plot locus specific MA plots between two conditions
 
-    def __init__(self, first, last, pay):
-        try:
-            self.first = first.capitalize()
-            self.last = last.capitalize()
-        except Exception as expt:
-            print("first and/or last name not valid: {}".format(expt))
-        try:
-            self.pay = float(pay)
-        except ValueError as err:
-            print("payment must be a number: {}".format(err))
+How to implement this:
 
-        self.tot_promotions = 0
-        self.record_promotions = {}
+1. Create a RELACS object (class). The object is initialized using the yml config file for the ChIP-seq snakePipe.
+2. method get_relacs_scaling_factors(bed_file=None, **kwargs) --> compute relacs scaling factors. By default, sf
+are computed using the total number of reads mapped to each sample according to specific constraints
+(see deeptools.countReadsPerBin). If a bed file is specified, the sf are computed based on the number of reads
+mapping to the bed regions.
 
-    @property
-    def email(self):
-        return "{}@ie-freiburg.mpg.de".format(self.last.lower())
+"""
 
-    def fullname(self):
-        return "{} {}".format(self.first, self.last)
+import os
+import yaml
+import logging
+from deeptools.countReadsPerBin import CountReadsPerBin as crpb
 
-    def raise_pay(self, ra=None):
-        self.tot_promotions +=1
 
-        if ra:
-            self.record_promotions[self.tot_promotions] = ra
-            self.pay = int(self.pay * ra)
+def process_yaml(config_yml, key_name=None):
+    """
+    Import yaml file. If key_name is provided, return value associated with key_name
+    otherwise, return the whole file content.
+    """
+    try :
+        with open(config_yml, 'r') as config:
+            dict_config = yaml.safe_load(config)
+        if key_name:
+            return dict_config[key_name]
         else:
-            self.record_promotions[self.tot_promotions] = self.raise_amt
-            self.pay = int(self.pay * self.raise_amt)
+            return dict_config
+    except Exception as exe:
+        print("Something is wrong with your config file: {}".format(exe))
+
+def get_all_bams(config_dict):
+    all_bams = list(set([sample for sample in config_dict.keys()])) + \
+                list(set(config_dict[sample]["control"] for sample in config_dict.keys()))
+    return all_bams
+
+
+def check_format_yaml():
+    # TODO: check that config_yml provided is in in the correct format
+    pass
+
+
+class makeRelacsObject:
+
+    def __init__(self, config_yml, experiment_name="GenericRelacsObject", snakePipes_config=None):
+        """ initializer of relacs object """
+        # 2. check that bam files exist and are indexed
+        self.experiment_name = experiment_name
+        self.config_yml = process_yaml(config_yml,"chip_dict")
+        self.snakePipes_config = snakePipes_config
+        if snakePipes_config:
+            self.base_dir = process_yaml(snakePipes_config, "outdir")
+            self.check_paths()
+        else:
+            self.base_dir = process_yaml(config_yml,"base_dir")
+            self.check_paths()
+
+    def check_paths(self):
+
+        all_bam_files = get_all_bams(self.config_yml)
+
+        if self.snakePipes_config:
+            file_list = [os.path.join(self.base_dir, "filtered_bam/{}.filtered.bam".format(sample)) for sample in all_bam_files]
+        else:
+            file_list = [os.path.join(self.base_dir, "{}.bam".format(sample)) for sample in all_bam_files]
+
+        ### check that bam files are present
+        check_file_path = [os.path.isfile(f_) for f_ in file_list]
+        if all(check_file_path):
+            # make logging info
+            print("all bam files are present")
+        else:
+            print("some files are not present: {}".format([file_list[i] for i, val in enumerate(check_file_path) if not val]))
+
+        ### check that indices are present
+        check_presence_indices = [os.path.isfile("{}.bai".format(f_)) for f_ in file_list]
+        if all(check_file_path):
+            # TODO: substitute print statments with proper logging
+            print("all indexes are present")
+        else:
+            print("some files indexes are not present: {}".format(["{}.bai".format(file_list[i]) for i, val in enumerate(check_file_path) if not val]))
+
+
+
+
+        # make sure paths exist
+
+
+
+
+
+
+
+    def __repr__(self):
+        return self.experiment_name
+
+    def __str__(self):
+        return self.experiment_name
+
+    def as_dataFrame(self):
+        pass
+
 
 def main():
-    employee = Employees("FRANCESCO","FERRARI",3000)
-    # employee = Employees(20,"FERRARI","france")
-    print(employee.first, employee.last, employee.email, employee.pay)
-    print(employee.tot_promotions, employee.record_promotions)
-    employee.raise_pay()
-    print(employee.pay, employee.tot_promotions, employee.record_promotions)
-    employee.raise_pay(1.1)
-    print(employee.pay, employee.tot_promotions, employee.record_promotions)
-
+    pass
 
 if __name__ == "__main__":
     main()
--- tests/test_relacs.py
+++ tests/test_relacs.py
@@ -1,37 +1,37 @@
 import unittest
 from relacs import relacs as rl
 
-class testCalc(unittest.TestCase):
-
-    def test_add(self):
-        self.assertEqual(rl.add_nums(10,5),15.0)
-        self.assertEqual(rl.add_nums(-1,1),0.0)
-        self.assertEqual(rl.add_nums(-1,-1),-2.0)
-
-    def test_divide(self):
-        self.assertEqual(rl.divide_nums(10,5),2)
-        self.assertEqual(rl.divide_nums(-1,1),-1)
-        self.assertEqual(rl.divide_nums(-1,-1),1)
-        self.assertEqual(rl.divide_nums(5,2),2.5)
-
-# test for errors to be correctly raised
-        self.assertRaises(ValueError, rl.divide_nums, 5, 0)
-        with self.assertRaises(ValueError):
-            rl.divide_nums(5, 0)
-
-class testEmployee(unittest.TestCase):
-
-    def setUp(self):
-        self.emp1 = rl.Employees("Francesco","Ferrari",1000)
-        self.emp2 = rl.Employees("ANGELICA","gRAY",1000)
-
-    def tearDown(self):
-        pass
-
-    def test_email(self):
-        self.assertEqual(self.emp1.email, "ferrari@ie-freiburg.mpg.de")
-        self.assertEqual(self.emp2.email, "gray@ie-freiburg.mpg.de")
-
+# class testCalc(unittest.TestCase):
+#
+#     def test_add(self):
+#         self.assertEqual(rl.add_nums(10,5),15.0)
+#         self.assertEqual(rl.add_nums(-1,1),0.0)
+#         self.assertEqual(rl.add_nums(-1,-1),-2.0)
+#
+#     def test_divide(self):
+#         self.assertEqual(rl.divide_nums(10,5),2)
+#         self.assertEqual(rl.divide_nums(-1,1),-1)
+#         self.assertEqual(rl.divide_nums(-1,-1),1)
+#         self.assertEqual(rl.divide_nums(5,2),2.5)
+#
+# # test for errors to be correctly raised
+#         self.assertRaises(ValueError, rl.divide_nums, 5, 0)
+#         with self.assertRaises(ValueError):
+#             rl.divide_nums(5, 0)
+#
+# class testEmployee(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.emp1 = rl.Employees("Francesco","Ferrari",1000)
+#         self.emp2 = rl.Employees("ANGELICA","gRAY",1000)
+#
+#     def tearDown(self):
+#         pass
+#
+#     def test_email(self):
+#         self.assertEqual(self.emp1.email, "ferrari@ie-freiburg.mpg.de")
+#         self.assertEqual(self.emp2.email, "gray@ie-freiburg.mpg.de")
+#
 
 if __name__ == "__main__":
     unittest.main()
